ffdc748a727959738b774fa2ab00baef68930a95
Awadje <michaelawad85@gmail.com>
2017-04-18 16:20:38 +0200

    Sign in is now required

--- config/routes.rb
+++ config/routes.rb
@@ -9,23 +9,22 @@ Rails.application.routes.draw do
   get "admin", to:  "pages#admin"
 
 
-    scope '/admin' do
-      resources :user
-    end
-
-    namespace :user do
+  authenticate :user do
+    scope "/user" do
       resources :days
-  end
-
-
-
-    namespace :admin do
-        resources :days
-  end
-
-        namespace :admin do
-            resources :users
     end
+  end
 
+  authenticate :admin do
+    scope "/admin" do
+      resources :days
+    end
+  end
+
+  authenticate :admin do
+    scope "/admin" do
+      resources :users
+      end
+    end
 
   end
