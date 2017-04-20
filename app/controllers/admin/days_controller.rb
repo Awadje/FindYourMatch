63f59de9c36adaca1878959364c5ea41c09d4634
Awadje <michaelawad85@gmail.com>
2017-04-20 10:39:17 +0200

    re-arranging filestructure controllers to work with namespace

---
+++ app/controllers/admin/days_controller.rb
@@ -0,0 +1,36 @@
+class DaysController < ApplicationController
+  def index
+    @days = Day.all
+  end
+
+  def create
+    @day = Day.new(day_params)
+  if @day.save
+     render 'new'
+     #matcher
+   end
+  end
+
+  def show
+     @day = Day.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+private
+  def day_params
+        params.require(:day).permit(:day, :user_id, :match)
+      end
+
+      # def matcher
+      #    @matching = User.pluck(:email).shuffle
+      #   [@matching.shift, @matching.pop]
+      # end
+
+
+
+  end
