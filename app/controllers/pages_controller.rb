460762633af447da2d127ba3c6e0fae92b2873a7
Awadje <michaelawad85@gmail.com>
2017-04-20 10:43:00 +0200

    Clear pages controller since it is not used at the moment

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,2 @@
 class PagesController < ApplicationController
-  def home
-  end
-
-  def student
-    @days = Day.all
-  end
-
-  def admin
-    @users = User.all
-      @days = Day.all
-  end
-
-  def user
-  end
-
 end
