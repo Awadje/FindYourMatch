7c4ccf7155c4d6e32b9d4d77e5fbc444349cfd88
Awadje <michaelawad85@gmail.com>
2017-04-20 20:02:39 +0200

    Experimenting with Serializers

--- app/controllers/admin/days_controller.rb
+++ app/controllers/admin/days_controller.rb
@@ -1,5 +1,10 @@
 class Admin::DaysController < ApplicationController
  before_filter :authorized?
+ include ActiveModel::Serialization::JSON
+
+attr_accessor :match
+
+
 
   def index
     @days = Day.all
@@ -31,6 +36,7 @@ def authorized?
     redirect_to root_path
   end
 end
+
 
   def day_params
         params.require(:day).permit(:day, :user_id, :match)
