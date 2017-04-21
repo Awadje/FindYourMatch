0fc06dade5142bb46e6cd6e9d43dcb30f271ba17
Awadje <michaelawad85@gmail.com>
2017-04-21 20:47:11 +0200

    Added foreign key columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,31 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419120431) do
+ActiveRecord::Schema.define(version: 20170421183216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "admins", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.integer  "days_id"
-    t.integer  "user_id"
-    t.index ["days_id"], name: "index_admins_on_days_id", using: :btree
-    t.index ["email"], name: "index_admins_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
-    t.index ["user_id"], name: "index_admins_on_user_id", using: :btree
-  end
 
   create_table "days", force: :cascade do |t|
     t.date     "day"
@@ -42,6 +21,8 @@ ActiveRecord::Schema.define(version: 20170419120431) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "match"
+    t.integer  "student_a"
+    t.integer  "student_b"
     t.index ["user_id"], name: "index_days_on_user_id", using: :btree
   end
 
@@ -65,8 +46,6 @@ ActiveRecord::Schema.define(version: 20170419120431) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_foreign_key "admins", "days", column: "days_id"
-  add_foreign_key "admins", "users"
   add_foreign_key "days", "users"
   add_foreign_key "users", "days", column: "days_id"
 end
