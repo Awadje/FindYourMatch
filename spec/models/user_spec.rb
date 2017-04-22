9ee16d878be1c06f435a71d1dd0e002a826fb81a
Awadje <michaelawad85@gmail.com>
2017-04-22 20:18:02 +0200

    Create test for admin&student de and promotion

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:student1) { create :user }
+  let(:student2) { create :user }
+  let(:student3) { create :user }
+  let(:admin) { create :user, admin: true }
+
+  describe "promote or demote" do
+    it "upgrades a user to admin" do
+      student1.upgrade
+      expect(student1.admin?).to eq(true)
+    end
+    it "demotes a admin to user" do
+      admin.demote
+      expect(admin.admin?).to eq(false)
+    end
 end
