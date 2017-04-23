22b057980cb3baf6bde6465c75786e09dac43fab
Awadje <michaelawad85@gmail.com>
2017-04-23 19:16:41 +0200

    Made the model specs test the correct things

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe User, type: :model do
   let(:student2) { create :user }
   let(:student3) { create :user }
   let(:admin) { create :user, admin: true }
+  let(:day1) { create :day, users: [student1, student2] }
+
 
   describe "User is not an admin" do
     it "Admin is false" do
@@ -15,5 +17,12 @@ RSpec.describe User, type: :model do
       admin
       expect(admin.admin?).to eq(true)
     end
+end
+
+    describe "student knows about his days" do
+      it "knows the day he's in" do
+        expect(student1.days).to include(day1)
+
   end
+end
 end
