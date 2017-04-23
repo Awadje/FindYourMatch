201c4b58ce0b82933707f55cb0802e37b8ed868a
Awadje <michaelawad85@gmail.com>
2017-04-23 12:21:44 +0200

    working on tests

--- spec/factories/days.rb
+++ spec/factories/days.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :day do
+    id     { Faker::Number }
     day    { Date.new }
     student_a { Faker::Number }
     student_b { Faker::Number }
--- spec/models/day_spec.rb
+++ spec/models/day_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Day, type: :model do
-  let!(:student1) { create :user, admin: false }
-  let!(:student2) { create :user, admin: false }
-  let!(:student3) { create :user, admin: false }
+  let!(:student1) { create :user, admin: false, id: 1}
+  let!(:student2) { create :user, admin: false, id: 2}
+  let!(:student3) { create :user, admin: false, id: 3}
 
  context "a day knows about it's users" do
-   let(:day) { create :day, match: [student1, student3] }
+   let!(:day) { create :day, student_a: student1, student_b: student3 }
 
    it "has users" do
      expect(day.users).to include(student1)
