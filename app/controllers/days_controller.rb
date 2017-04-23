c5c7203f6e7e35fd8d6504ec86e57d7d4a79308a
Awadje <michaelawad85@gmail.com>
2017-04-23 17:13:37 +0200

    Minor fix in student view

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -5,17 +5,17 @@ class DaysController < ApplicationController
     days_index = Day.all
 
     studentdays = days_index.select do |day|
-      day.student_a == current_user.id.to_s || day.student_b == current_user.id.to_s
+      day.student_a == current_user.id || day.student_b == current_user.id
 
   end
 
-  @studentdays = days_index.select do |day|
+  @studentdays = studentdays.select do |day|
     day.day == Date.today
   end
 
-  @studenthistory  = days_index.select do |day|
-    day.day < Date.today
-  end
+    @studenthistory = studentdays.select do |day|
+      day.day <= Date.today
+    end
 
 end
 end
